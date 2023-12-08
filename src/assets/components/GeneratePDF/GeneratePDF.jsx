9cfbb8c012adf8a735bdee8f870a5f5cb7874ca9
David <soaresdavid411@gmail.com>
2023-12-08 09:09:53 -0300

    fix: browser compatibility issue resolved.

--- src/assets/components/GeneratePDF/GeneratePDF.jsx
+++ src/assets/components/GeneratePDF/GeneratePDF.jsx
@@ -4,20 +4,16 @@ import { Button } from "react-bootstrap";
 const GeneratePDF = () => {
     const generatePDF = () => {
       const doc = new jsPDF();
-  
-      // Adicione o conteúdo ao PDF
       doc.text('Olá, este é o meu PDF!', 10, 10);
-  
-      // Obtenha o conteúdo do PDF como um data URL
-      const pdfData = doc.output('datauristring');
-  
-      // Abra o PDF em uma nova aba do navegador
-      window.open(pdfData, '_blank');
+      const pdfBlob = doc.output('blob');
+      const blobUrl = URL.createObjectURL(pdfBlob);
+      window.open(blobUrl, '_blank');
     };
+  
 
     return (
        <Button variant="success" onClick={generatePDF}> Visualizar PDF </Button>
     );
 }
 
-export default GeneratePDF;
+export default GeneratePDF; 
